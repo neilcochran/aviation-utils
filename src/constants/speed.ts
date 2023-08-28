9f5b504b8d1b5260722817607e6951f447f00dbf
neilcochran <dev.neil.cochran@pm.me>
2023-08-27 20:31:58 -0400

    add ICAOIdentifierPrefix and constants for all values

---
+++ src/constants/speed.ts
@@ -0,0 +1,9 @@
+/**
+ * Enum representing the various speed units and their official abbreviations
+ */
+export enum SpeedUnits {
+    KNOTS = 'KT',
+    KILOMETERS_PER_HR = 'KMH',
+    METERS_PER_SEC = 'MPS',
+    MILES_PER_HOUR = 'MPH'
+}
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-(function main() {
-    console.log('This project was created using mk-ts-app!');
-})();
