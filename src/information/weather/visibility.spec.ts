dbe8fa98f877c59e6d07078398c0e04c1dcef85b
neilcochran <dev.neil.cochran@pm.me>
2023-08-31 22:51:01 -0400

    add varying visibility fields and tests

--- src/information/weather/visibility.spec.ts
+++ src/information/weather/visibility.spec.ts
@@ -10,12 +10,32 @@ describe('enforce Visibility constructor validation', () => {
         assert.equal(visibility.visibility, 5);
         assert.equal(visibility.distanceUnits, DistanceUnits.STATUTE_MILES);
     });
+    it('construct a valid Visibility object with variable visibility', () => {
+        const visibility = new Visibility(700, DistanceUnits.METERS, 1000, 3000);
+        assert.ok(visibility);
+        assert.equal(visibility.visibility, 700);
+        assert.equal(visibility.distanceUnits, DistanceUnits.METERS);
+        assert.equal(visibility.varyingVisibilityFrom, 1000);
+        assert.equal(visibility.varyingVisibilityTo, 3000);
+    });
 
     //Negative test cases
     it('fail to construct due to invalid distance value', () => {
-        assert.throws(() => new Visibility(-1, DistanceUnits.METERS), {
+        assert.throws(() => new Visibility(-1, DistanceUnits.KILOMETERS), {
             name: 'Error',
             message: 'visibility cannot be negative: -1'
+        });
+    });
+    it('fail to construct due to only passing varyingVisibilityFrom value', () => {
+        assert.throws(() => new Visibility(2000, DistanceUnits.FEET, 500), {
+            name: 'Error',
+            message: 'when using variable visibility, both varyingVisibilityFrom and varyingVisibilityTo must be given'
+        });
+    });
+    it('fail to construct due to only passing varyingVisibilityTo value', () => {
+        assert.throws(() => new Visibility(2000, DistanceUnits.FEET, undefined, 500), {
+            name: 'Error',
+            message: 'when using variable visibility, both varyingVisibilityFrom and varyingVisibilityTo must be given'
         });
     });
 });
--- src/information/weather/visibility.ts
+++ src/information/weather/visibility.ts
@@ -1,9 +1,31 @@
 import { DistanceUnits } from '../../common/units';
 
+/**
+ *
+ */
 export class Visibility {
-    constructor(public visibility: number, public distanceUnits: DistanceUnits){
+
+    /**
+     *
+     * @param visibility
+     * @param distanceUnits
+     * @param varyingVisibilityFrom
+     * @param varyingVisibilityTo
+     */
+    constructor(public visibility: number, public distanceUnits: DistanceUnits, public varyingVisibilityFrom?: number, public varyingVisibilityTo?: number){
         if(visibility < 0) {
             throw new Error(`visibility cannot be negative: ${visibility}`);
+        }
+        if((varyingVisibilityFrom != undefined && varyingVisibilityTo == undefined) || (varyingVisibilityFrom == undefined && varyingVisibilityTo != undefined)) {
+            throw new Error('when using variable visibility, both varyingVisibilityFrom and varyingVisibilityTo must be given');
+        }
+        if(varyingVisibilityFrom != undefined && varyingVisibilityTo != undefined) {
+            if(varyingVisibilityFrom < 0) {
+                throw new Error(`varyingVisibilityFrom cannot be negative: ${varyingVisibilityFrom}`);
+            }
+            if(varyingVisibilityTo < 0) {
+                throw new Error(`varyingVisibilityTo cannot be negative: ${varyingVisibilityTo}`);
+            }
         }
     }
 }
